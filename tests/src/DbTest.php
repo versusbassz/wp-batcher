4aa8801b32528ec30fbb9a26686c57caa9103b3b
Vladimir Sklyar <versus.post@gmail.com>
2022-01-02 17:28:36 +0300

    Add the test to check data consistency of a database after a loop is finished

--- tests/src/DbTest.php
+++ tests/src/DbTest.php
@@ -82,8 +82,6 @@ class DbTest extends \WP_UnitTestCase {
 
 		$memory_usage_before = memory_get_usage();
 
-		// ===========
-
 		$iterable = (new CallbackBatcher())
 			->set_fetcher( function ( $paged, $posts_per_page ) {
 				$query = new \WP_Query( [
@@ -96,22 +94,111 @@ class DbTest extends \WP_UnitTestCase {
 
 				return $query->posts;
 			} )
-//			->use_suspend_cache_addition()
 			->set_items_per_page( 50 );
 
 		$this->log_value( 'before loop', memory_get_usage() );
 
+		$iterations_count = 0;
+
 		foreach ( $iterable as $post ) {
+			++$iterations_count;
 			has_post_thumbnail( $post->ID);
 		}
-
-		// ===========
 
 		$memory_usage_after = memory_get_usage();
 
 		$this->log_value( 'end', $memory_usage_after );
 		$this->log_value( 'diff', $memory_usage_after - $memory_usage_before );
 
+		$this->assertSame( $posts_qty, $iterations_count );
+
 		$this->assertLessThan( 500 * 1000, $memory_usage_after - $memory_usage_before );
+	}
+
+	public function testDataConsistency() {
+		$this->log( '========================================' );
+
+		wp_suspend_cache_addition( true );
+
+		$posts_qty = 30;
+		self::factory()->post->create_many( $posts_qty );
+
+		wp_suspend_cache_addition( false );
+
+		$this->assertSame( (string) $posts_qty, $this->getPostsCount() );
+
+		$iterable = (new CallbackBatcher())
+			->set_fetcher( function ( $paged, $posts_per_page ) {
+				$query = new \WP_Query( [
+					'post_type' => [ 'post' ],
+					'post_status' => [ 'any' ],
+
+					'paged' => $paged,
+					'posts_per_page' => $posts_per_page,
+					'orderby' => 'ID',
+					'order' => 'ASC',
+				] );
+
+				return $query->posts;
+			} )
+			->set_items_per_page( 5 );
+
+		$memory_usage_before = memory_get_usage();
+
+		$this->log_value( 'before loop', $memory_usage_before );
+
+		$iterations_count = 0;
+
+		foreach ( $iterable as $post ) {
+			++$iterations_count;
+			$update_result = update_post_meta( $post->ID, 'test_field', 'test_value' );
+		}
+
+		$memory_usage_after = memory_get_usage();
+
+		$this->log_value( 'end', $memory_usage_after );
+		$this->log_value( 'diff', $memory_usage_after - $memory_usage_before );
+
+		$this->assertSame( $posts_qty, $iterations_count );
+
+		// there is no posts without the field
+		$posts = new \WP_Query( [
+			'post_type' => [ 'post' ],
+			'post_status' => [ 'any' ],
+
+			'meta_query' => [
+				[
+					'key' => 'test_field',
+					'compare' => 'NOT EXISTS',
+				],
+			],
+
+			'nopaging' => true,
+			'orderby' => 'ID',
+			'order' => 'ASC',
+			'fields' => 'ids',
+		] );
+
+		$this->assertSame( 0, $posts->found_posts );
+
+		// there are N posts with the field
+		$posts = new \WP_Query( [
+			'post_type' => [ 'post' ],
+			'post_status' => [ 'any' ],
+
+			'meta_query' => [
+				[
+					'key' => 'test_field',
+					'value' => 'test_value',
+				],
+			],
+
+			'nopaging' => true,
+			'orderby' => 'ID',
+			'order' => 'ASC',
+			'fields' => 'ids',
+		] );
+
+		$this->assertSame( 30, $posts->found_posts );
 	}
 }
